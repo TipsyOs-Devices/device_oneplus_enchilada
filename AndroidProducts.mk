LineageOS/android_device_oneplus_enchilada

enchilada: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch  [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,3 +16,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_enchilada.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_enchilada-user \
+    lineage_enchilada-userdebug \
+    lineage_enchilada-eng

TipsyOs-Devices/device_oneplus_enchilada

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,3 +16,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/carbon_enchilada.mk
+
+COMMON_LUNCH_CHOICES := \
+    carbon_enchilada-user \
+    carbon_enchilada-userdebug \
+    carbon_enchilada-eng