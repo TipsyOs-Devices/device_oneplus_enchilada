a59a384c05ad7d0fba286f45fb8b032f90f997f6
martinusbe <martinusbe@gmail.com>
2020-05-02 14:34:27 +0200

    enchilada: build vendor/gapps

--- tipsy_enchilada.mk
+++ tipsy_enchilada.mk
@@ -18,6 +18,9 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# gapps
+$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
+
 # Inherit from enchilada device
 $(call inherit-product, device/oneplus/enchilada/device.mk)
 
